4e8ef52a40176a3e103d4e278a2e4a362b0762c1
Soulmate-ing <1599544571@qq.com>
2025-08-19 12:49:51 +0800

    Add files via upload

---
+++ ObjectPool/PoolManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PoolManager : MonoBehaviour
+{
+    public List<GameObject> poolPrefabs;
+    public List<ObjectPool<GameObject>> poolEffectList = new List<ObjectPool<GameObject>>();
+
+    private Queue<GameObject> soundQueue = new Queue<GameObject>();
+    private void Start()
+    {
+        CreatePool();
+    }
+    private void OnEnable()
+    {
+        EventHandler.ParticleEffectEvent += OnParticleEffectEvent;
+        EventHandler.InitSoundEffect += InitSoundEffect;
+    }
+    private void OnDisable()
+    {
+        EventHandler.ParticleEffectEvent -= OnParticleEffectEvent;
+        EventHandler.InitSoundEffect -= InitSoundEffect;
+    }
+
+    /// <summary>
+    /// 生成对象池
+    /// </summary>
+    private void CreatePool()
+    {
+        foreach (GameObject item in poolPrefabs)
+        {
+            Transform parent = new GameObject(item.name).transform;
+            parent.SetParent(transform);
+
+            var newPool = new ObjectPool<GameObject>(
+                () => Instantiate(item, parent),
+                e => { e.SetActive(true); },
+                e => { e.SetActive(false); },
+                e => { Destroy(e); }
+            );
+
+            poolEffectList.Add(newPool);
+        }
+    }
+    public void OnParticleEffectEvent(ParticleEffectType effectType, Vector3 pos)
+    {
+        //WORKFLOW:根据特效补全
+        ObjectPool<GameObject> objPool = effectType switch
+        {
+            ParticleEffectType.LeavesFalling => poolEffectList[0],
+            ParticleEffectType.Rock => poolEffectList[1],
+            _ => null,
+        };
+
+        GameObject obj = objPool.Get();
+        obj.transform.position = pos;
+        StartCoroutine(ReleaseRoutine(objPool, obj));
+    }
+
+    private IEnumerator ReleaseRoutine(ObjectPool<GameObject> pool, GameObject obj)
+    {
+        yield return new WaitForSeconds(1.5f);
+        pool.Release(obj);
+    }
+
+    private void CreateSoundPool()
+    {
+        var parent = new GameObject(poolPrefabs[2].name).transform;
+        parent.SetParent(transform);
+
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject newObj = Instantiate(poolPrefabs[2], parent);
+            newObj.SetActive(false);
+            soundQueue.Enqueue(newObj);
+        }
+    }
+
+
+    private GameObject GetPoolObject()
+    {
+        if (soundQueue.Count < 2)
+            CreateSoundPool();
+        return soundQueue.Dequeue();
+    }
+    private void InitSoundEffect(SoundDetails soundDetails)
+    {
+        var obj = GetPoolObject();
+        obj.GetComponent<Sound>().SetSound(soundDetails);
+        obj.SetActive(true);
+        StartCoroutine(DisableSound(obj, soundDetails.soundClip.length));
+    }
+
+    private IEnumerator DisableSound(GameObject obj, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        obj.SetActive(false);
+        soundQueue.Enqueue(obj);
+    }
+}
